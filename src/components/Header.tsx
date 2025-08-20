64f7d3380c81755f8096fb020c403bd074d7381f
Ditt <dittdemokrati01@gmail.com>
2025-08-20 12:19:24 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Header>
    
    with the

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -63,7 +63,7 @@ export function Header() {
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 items-center">
         <div className="mr-4 flex">
-          <Link href="/" className="mr-6 flex items-center space-x-2">
+          <Link href="/" className="flex items-center space-x-2">
             <Vote className="h-6 w-6 text-primary" />
             <span className="font-bold">Ditt Demokrati</span>
           </Link>
