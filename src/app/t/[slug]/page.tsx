4e4c1fdb6619601b45a1eab28659f205361ff740
Ditt <dittdemokrati01@gmail.com>
2025-08-25 11:26:12 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <CardHeader classN

--- src/app/t/[slug]/page.tsx
+++ src/app/t/[slug]/page.tsx
@@ -157,7 +157,7 @@ function TopicCarousel({ topics, initialSlug }: { topics: Topic[], initialSlug: 
                 <div className="container mx-auto max-w-4xl px-4 py-4 md:py-8">
                   <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
                     <div className="lg:col-span-2 space-y-8">
-                      <Card className="bg-[hsl(222_52%_22%)] text-white">
+                      <Card className="bg-[hsl(222,47%,11%)] text-white">
                         <CardHeader className="p-4 md:p-6">
                           <h1 className="text-2xl md:text-3xl font-bold font-headline">{question}</h1>
                           {topic.background_md && (
