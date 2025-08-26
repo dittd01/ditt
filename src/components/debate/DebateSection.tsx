e1f26a39c5d195508bdf6c1a29669f46f1d7f486
Ditt <dittdemokrati01@gmail.com>
2025-08-26 15:57:19 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button variant="g

--- src/components/debate/DebateSection.tsx
+++ src/components/debate/DebateSection.tsx
@@ -1,5 +1,4 @@
 
-
 'use client';
 
 import { useState, useEffect, useMemo } from 'react';
@@ -308,7 +307,7 @@ export function DebateSection({ topicId, topicQuestion, initialArgs, lang, synth
                     <h3 className="text-xl font-semibold text-[hsl(var(--chart-2))]">
                         {t.for}
                     </h3>
-                    <Button variant="ghost" size="icon" onClick={() => handleAddArgument('for')} className="text-green-600 hover:text-green-700">
+                    <Button variant="ghost" size="icon" onClick={() => handleAddArgument('for')} className="text-[hsl(var(--chart-2))] hover:text-[hsl(var(--chart-2))]">
                         <PlusCircle className="h-5 w-5" />
                     </Button>
                 </div>
@@ -336,7 +335,7 @@ export function DebateSection({ topicId, topicQuestion, initialArgs, lang, synth
                     <h3 className="text-xl font-semibold text-destructive">
                         {t.against}
                     </h3>
-                     <Button variant="ghost" size="icon" onClick={() => handleAddArgument('against')} className="text-red-600 hover:text-red-700">
+                     <Button variant="ghost" size="icon" onClick={() => handleAddArgument('against')} className="text-destructive hover:text-destructive/80">
                         <PlusCircle className="h-5 w-5" />
                     </Button>
                 </div>
